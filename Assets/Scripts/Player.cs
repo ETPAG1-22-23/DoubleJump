7bd4b06453d27a2edc892dbdacb238f26d59b661
jmorquin <j.morquin@mtp.ecolescreatives.com>
2023-02-01 11:21:39 +0100

    Saut

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -9,17 +9,21 @@ public class Player : MonoBehaviour
     Animator animController;
     float horizontal_value;
     Vector2 ref_velocity = Vector2.zero;
-   
+
 
     float jumpForce = 12f;
 
     [SerializeField] float moveSpeed_horizontal = 400.0f;
     [SerializeField] bool is_jumping = false;
     [SerializeField] bool can_jump = false;
-    [Range(0, 1)][SerializeField] float smooth_time = 0.5f;
-    [SerializeField] bool canDoubleJump = false;
-    public bool IsGrounded;
+    [Range(0, 1)] [SerializeField] float smooth_time = 0.5f;
 
+
+
+    [SerializeField] bool IsGrounded = false;
+    [SerializeField] int CountJump = 2;
+    private int LastPressedJumpTime = 0;
+    private int LastOnGroundTime = 0;
 
     // Start is called before the first frame update
     void Start()
@@ -35,58 +39,68 @@ public class Player : MonoBehaviour
     {
         horizontal_value = Input.GetAxis("Horizontal");
 
-        if(horizontal_value > 0) sr.flipX = false;
+        if (horizontal_value > 0) sr.flipX = false;
         else if (horizontal_value < 0) sr.flipX = true;
-        
+
         animController.SetFloat("Speed", Mathf.Abs(horizontal_value));
 
-       if(IsGrounded())
+
+        if (Input.GetKeyDown(KeyCode.Space) && CountJump > 0)
         {
-            canDoubleJump = true;
+            Jump();
+
         }
-   
-        if (Input.GetButtonDown("Jump") && can_jump)
-         if (IsGrounded())
-        {
-            is_jumping = true;
-            animController.SetBool("Jumping", true);
-          }else if (canDoubleJump())
-             {
-                is_jumping = true;
-                animController.SetBool("Jumping", true);
-                canDoubleJump = false;
-             }
-        }
-        
-           
-        
-       
-        
     }
+
+
+    void Jump()
+    {
+        // Garantit que nous ne pouvons pas appeler Jump plusieurs fois à partir d'une seule pression
+        LastPressedJumpTime = 0;
+        LastOnGroundTime = 0;
+        CountJump -= 1;
+
+        // On augmente la force appliquée si on tombe
+        // Cela signifie que nous aurons toujours l'impression de sauter le même montant
+        float force = jumpForce;
+        if (rb.velocity.y < 0)
+            force -= rb.velocity.y;
+
+
+        rb.AddForce(Vector2.up * force, ForceMode2D.Impulse);
+
+    }
+
+
+
+
+
     void FixedUpdate()
     {
-        if (is_jumping && can_jump)
-        {           
-            is_jumping = false;
-            rb.AddForce(new Vector2(0, jumpForce), ForceMode2D.Impulse);
-            can_jump = false;
-        }
+
         Vector2 target_velocity = new Vector2(horizontal_value * moveSpeed_horizontal * Time.fixedDeltaTime, rb.velocity.y);
         rb.velocity = Vector2.SmoothDamp(rb.velocity, target_velocity, ref ref_velocity, 0.05f);
-        
+
     }
     private void OnTriggerStay2D(Collider2D collision)
     {
-        can_jump = true;
+
         animController.SetBool("Jumping", false);
+
     }
+
     private void OnTriggerEnter2D(Collider2D collision)
-    {   
-        animController.SetBool("Jumping", false);        
-    }
-
-    void jump ()
     {
-
+        animController.SetBool("Jumping", false);
+        IsGrounded = true;
+        CountJump = 2; //reset double saut quand on touche le sol
     }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        animController.SetBool("Jumping", true);
+        IsGrounded = false;
+    }
+
+
 }
